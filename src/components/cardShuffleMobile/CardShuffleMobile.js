d5ec4c4f035c183ec55cf67b7b42cbeac87cd8ae
Alex Murys <amurys@visual-engin.com>
2021-11-18 11:47:49 +0100

    Updates

--- src/components/cardShuffleMobile/CardShuffleMobile.js
+++
@@ -1,9 +0,0 @@
-/* import { useContext } from "react";
-import CardListContext from "../../context/CardListContext";
-
-const CardShuffleMobile = (props) => {
-  const { isMobile, setIsMobile } = useContext(CardListContext);
-};
-
-export default CardShuffleMobile;
- */
