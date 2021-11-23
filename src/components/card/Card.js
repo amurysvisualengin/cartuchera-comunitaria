d6a1721d41c5a63b8812deb77c0856e936d1af31
Alex Murys <amurys@visual-engin.com>
2021-11-23 12:35:18 +0100

    heigth

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -82,7 +82,7 @@ const Card = (props) => {
       >
         <p className="custom-font text-right items-start">{cardNumber}</p>
         <img
-          className={` ${isMobile ? "mt-4 w-80 h-96" : "w-36 h-60"}`}
+          className={` ${isMobile ? "mt-4 w-40 h-48" : "w-36 h-60"}`}
           src={img}
           alt=""
           srcSet=""
