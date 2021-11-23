0802088d02d5435a7e27ba350402991a892e4c59
Alex Murys <amurys@visual-engin.com>
2021-11-23 12:31:42 +0100

    revirtiendo cambios

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -69,8 +69,8 @@ const Card = (props) => {
         onTouchEnd={handleTouchEnd}
         className={` ${
           isMobile
-            ? isTablet < 320
-              ? " ml-5 mr-5 h-40  p-16 card-item cursor-pointer"
+            ? isTablet < 376
+              ? " ml-5 mr-5 h-99.2 p-16 card-item cursor-pointer"
               : isTablet < 394
               ? " ml-5 mr-5 h-98 p-16 card-item cursor-pointer"
               : isTablet < 415
@@ -82,9 +82,7 @@ const Card = (props) => {
       >
         <p className="custom-font text-right items-start">{cardNumber}</p>
         <img
-          className={` ${
-            isMobile ? (isTablet <= 320 ? "w-52 h-40" : "w-36 h-60") : ""
-          }`}
+          className={` ${isMobile ? "mt-4 w-80 h-96" : "w-36 h-60"}`}
           src={img}
           alt=""
           srcSet=""
